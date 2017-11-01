11896017ef9eac1bdc56aa860f9353606c5de710
lipengdi <1966215865@qq.com>
2017-11-01 08:58:40 +0800

    提交

---
+++ js/login.js
@@ -0,0 +1,16 @@
+ var oHuoqu = document.getBitmapById("huoqu");
+ var oLogin2 = document.getElementById("oLogin2");
+ var oInput = ducument.getElementById("input");
+ 
+// oHuoqu.onclick = function(){
+//				
+//				var reg = /\d{11}/
+//			    var str = phone.value
+//				if(reg.test(str)){
+//				  oLogin2.style.display="block"
+//				}else{
+//				  alert("请输入正确的手机号")
+//				}
+//			 }
+
+  
---
+++ js/regsiter.js
@@ -0,0 +1,6 @@
+var oBtn = document.getElementById("btn");
+var oReg = document.getElementById("reg");
+var oSn = document.getElementById("sn");
+oBtn.onclick = function(){
+	oReg.style.display= "block";
+}
